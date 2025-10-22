135e476c00be43b3d63817dc899571eab1d5d062
Tolben <vitek.pasek@gmail.com>
2025-10-22 02:20:03 +0200

    chore(schema): add/update generated schema + README (umbrella sync)

--- schema/030_foreign_keys.sql
+++ schema/030_foreign_keys.sql
@@ -1,3 +1,3 @@
--- Auto-generated from schema-map.psd1 @ 1e83bb6 (2025-10-21T10:18:36+02:00)
+-- Auto-generated from schema-map.psd1 (map@1e83bb6)
 -- table: refunds
 ALTER TABLE refunds ADD CONSTRAINT fk_refunds_payment FOREIGN KEY (payment_id) REFERENCES payments(id) ON DELETE CASCADE;
